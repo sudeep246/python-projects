5d90993b24e9718723dee1f8ce5e73e7d32c49f9
sudeep santra <120881870+sudeep246@users.noreply.github.com>
2023-01-07 14:08:21 +0530

    Add files via upload

---
+++ scraprIDMB.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+
+import requests,openpyxl
+
+excel= openpyxl.Workbook()
+print(excel.sheetnames)
+
+sheet = excel.active 
+sheet.title = 'top rated movies'
+print(excel.sheetnames)
+
+sheet.append(['movie rank','movie name','year','imdb rating'])
+
+try:
+    source= requests.get('https://www.imdb.com/chart/top/')
+    source.raise_for_status()
+
+    soup=BeautifulSoup(source.text,'html.parser')
+    
+    movies= soup.find('tbody', class_="lister-list").findAll('tr')
+
+    for movie in movies:
+        name =movie.find('td',class_="titleColumn").a.text
+
+        rank=movie.find('td',class_="titleColumn").get_text(strip=True).split('.')[0]
+
+        year=movie.find('td',class_="titleColumn").span.text.strip('()')
+
+        rating=movie.find("td",class_='ratingColumn imdbRating').strong.text
+
+
+        print(rank,name,year,rating)
+        sheet.append([rank,name,year,rating])
+        
+
+
+except Exception as e:
+    print(e)
+
+excel.save('IMDb movie rating.xlsx')
